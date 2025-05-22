bbe7360d15e063a7dee6db50f953fd25f65a7c2d
datadiego <juandiegomariscal@gmail.com>
2025-05-22 13:31:56 +0200

    update

---
+++ build.js
@@ -0,0 +1,6 @@
+import nunjucks from 'nunjucks'
+import { createDirectoryIfNotExists } from './scripts/utils.js'
+import { buildAssets } from './scripts/generateHtml.js'
+createDirectoryIfNotExists('./dist')
+nunjucks.configure('views', { autoescape: true })
+buildAssets()
